8d0c7a12e265a1cb8cd24bbb691213a6c69bec0c
Tristan Jogminas <tristan.jogminas@gmail.com>
2017-12-20 00:31:51 -0800

    start urlLoad

--- SpotifyStuff.js
+++ SpotifyStuff.js
@@ -10,13 +10,14 @@ function Main() {
 
 function generateTrackList(songs,trackList) {
     songtxt = '';
+    token = generateToken
     if (songs.length > 50) {
         for (i = 0; i < 50; i++) {
             songtxt += songs[i] + ',';
         }
         songtxt = songtxt.substring(0,songtxt.length - 1);
         url = urlBuild(songtxt);
-        jasonText = urlLoad(url);
+        jasonText = urlLoad(url, token);
         //trackList += printJason(jasonText);
         //return generateTrackList(songs.substring(51,songs.length), trackList);
     } else {
@@ -25,7 +26,7 @@ function generateTrackList(songs,trackList) {
         }
         songtxt = songtxt.substring(0,songtxt.length - 1);
         url = urlBuild(songtxt);
-        jasonText = urlLoad(url);
+        jasonText = urlLoad(url, token);
         //trackList += printJason(jasonText);
         //return trackList;
     }
@@ -37,7 +38,7 @@ function urlBuild(songtxt) {
     return url;
 }
 
-function urlLoad(url) {
+function generateToken() {
     var xmlhttp = new XMLHttpRequest();
     var tokenURL = "https://accounts.spotify.com/api/token"
     xmlhttp.open("POST", tokenURL, false);
@@ -45,33 +46,23 @@ function urlLoad(url) {
     var client_id = 'd82d1ee7fe604b56ac800a4e9f8477e0';
     var client_secret = '79689b652261464f8c718b4613369ab2';
     var auth_req64 = window.btoa(client_id + ":"+ client_secret);
-    xmlhttp.setRequestHeader("Authorization", "Basic ZDgyZDFlZTdmZTYwNGI1NmFjODAwYTRlOWY4NDc3ZTA6Nzk2ODliNjUyMjYxNDY0ZjhjNzE4YjQ2MTMzNjlhYjI=");
+    xmlhttp.setRequestHeader("Authorization", "Basic " + auth_req64);
     xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xmlhttp.send("grant_type=client_credentials");
-    
     var data = xmlhttp.responseText;
     var token = JSON.parse(data).access_token;
-    alert(token);
+    return token;
 }
 
 
-
-
-//     auth_req64 = base64.b64encode((client_id + ':' + client_secret).encode('UTF-8'))
-//     auth_req_url = 'https://accounts.spotify.com/api/token'
-//     data_encoded = urllib.parse.urlencode({'grant_type':'client_credentials'}).encode('UTF-8')
-//     token_header = {'Authorization':'Basic '+auth_req64.decode()}
-
-//     //request_response = urllib.request.Request(auth_req_url, data_encoded, token_header)
-//     response = urllib.request.urlopen(request_response)
-//     access_token = json.load(response)['access_token']
-//     song_header = {'Authorization':'Bearer '+ access_token}
-
-//     request_response = urllib.request.Request(url, None, song_header)
-//     response = urllib.request.urlopen(request_response)
-//     jason_text = json.load(response)
-//     return jason_text
-// }
+function urlLoad(url, token){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, false);
+    xmlhttp.setRequestHeader("Authorization", "Bearer " + token);
+    xmlhttp.send()
+    alert(xmlhttp.responseText)
+    return xmlhttp.responseText
+}
 
 //
 
