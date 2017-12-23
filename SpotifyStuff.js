e2b18e17ce563f7bb39af04ed09e839546bce9c6
NeelNarvekar <weasel.nn@gmail.com>
2017-12-22 20:05:55 -0800

    radical changes

--- SpotifyStuff.js
+++ SpotifyStuff.js
@@ -4,17 +4,7 @@ function Main() {
     for (i = 0; i < URLList.length; i++) {
         URLList[i] = URLList[i].substring(14, URLList[i].length);
     }
-    var track_list = generateTrackList(URLList, []);
-    //alert(typeof track_list);
-    // track_list = track_list.split(",");
-    var screenWidth = screen.width/4;
-    var html = "<table><tr><td width=\""+screenWidth+"\"><b><u><font size=\"5\">Artists</font></u></b></td><td width=\""+screenWidth+"\"><b><u><font size=\"5\">Songs</font></u></b></td><td width=\""+screenWidth+"\"><b><u><font size=\"5\">Sample Music</font></u></b></td><td width=\""+screenWidth+"\"><b><u><font size=\"5\">Art</font></u></b></td></tr></table><br>";
-    var k = 1;
-    for (i = 3; i < track_list.length; i = i + 4) {
-            html += "<table><tr><td width=\""+screenWidth+"\">" + k + ". " + track_list[i-3] + "</td><td width=\""+screenWidth+"\">" + track_list[i-2] + "</td><td width=\""+screenWidth+"\">" + track_list[i-1] + "</td><td width=\""+screenWidth+"\">" + track_list[i] + "</td></tr></table><br>";
-            k = k + 1;
-    }
-    document.getElementById("testing").innerHTML = html;
+    generateTrackList(URLList, []);
 }
 
 function generateTrackList(songs,trackList) {
@@ -27,7 +17,7 @@ function generateTrackList(songs,trackList) {
         songtxt = songtxt.substring(0,songtxt.length - 1);
         url = urlBuild(songtxt);
         jasonText = urlLoad(url, token);
-        trackList += printJason(jasonText));
+        trackList += printJason(jasonText);
         return generateTrackList(songs.slice(50,songs.length+1), trackList);
     } else {
         for (i = 0; i < songs.length; i++) {
@@ -36,7 +26,7 @@ function generateTrackList(songs,trackList) {
         songtxt = songtxt.substring(0,songtxt.length - 1);
         url = urlBuild(songtxt);
         jasonText = urlLoad(url, token);
-        trackList += printJason(jasonText));
+        trackList += printJason(jasonText);
         return trackList;
     }
 }
@@ -107,21 +97,16 @@ function printJason(jasonText){
         totalTrackElements.push(elements);
         //alert(typeof totalTrackElements);
     }
-
-
-//     var x = new Array(10);
-// for (var i = 0; i < 10; i++) {
-//   x[i] = new Array(20);
-// }
-// x[5][12] = 3.0;
-// <<<<<<< HEAD
-//     //trackList = String(trackList);
-//     // trackList = trackList.join('<br>');
-// =======
-//     // trackList = trackList.join('<br>');
-// >>>>>>> e26d6b0f0773d34db7d67a0c12e383e0e034aaa7
-    //console.log(totalTrackElements)
-    return totalTrackElements;
+    //alert(typeof track_list);
+    // track_list = track_list.split(",");
+    var screenWidth = screen.width/4;
+    var html = "<table><tr><td width=\""+screenWidth+"\"><b><u><font size=\"5\">Artists</font></u></b></td><td width=\""+screenWidth+"\"><b><u><font size=\"5\">Songs</font></u></b></td><td width=\""+screenWidth+"\"><b><u><font size=\"5\">Sample Music</font></u></b></td><td width=\""+screenWidth+"\"><b><u><font size=\"5\">Art</font></u></b></td></tr></table><br>";
+    var k = 1;
+    for (i = 3; i < totalTrackElements.length; i = i + 4) {
+            html += "<table><tr><td width=\""+screenWidth+"\">" + k + ". " + totalTrackElements[i-3] + "</td><td width=\""+screenWidth+"\">" + totalTrackElements[i-2] + "</td><td width=\""+screenWidth+"\">" + totalTrackElements[i-1] + "</td><td width=\""+screenWidth+"\">" + totalTrackElements[i] + "</td></tr></table><br>";
+            k = k + 1;
+    }
+    document.getElementById("testing").innerHTML = html;
 }
 
     
